e8fb2f929c64c53e3e51c6338b80c5143d306861
hz6 <hz6@ualberta.ca>
2020-07-06 14:39:46 -0600

    july 6

--- MaximumProductSubarray.java
+++ MaximumProductSubarray.java
@@ -1,5 +1,17 @@
 public class MaximumProductSubarray {
 	public int maxProduct(int[] nums) {
-		return 0;
+		if (nums == null || nums.length == 0) {
+			return 0;
+		}
+		int maxCurr = nums[0];
+		int maxGlobal = maxCurr;
+
+		for (int i = 1; i < nums.length; i++) {
+			maxCurr = Math.max(nums[i], maxCurr + nums[i]);
+			maxGlobal = Math.max(maxGlobal, maxCurr);
+		}
+
+		return maxGlobal;
 	}
 }
+// https://leetcode.com/problems/maximum-subarray/
