a367e73f597a59a998fb99a10fc1face159ca713
hz6 <hz6@ualberta.ca>
2020-11-15 11:43:12 -0700

    problem correction

--- MaximumProductSubarray.java
+++ MaximumProductSubarray.java
@@ -1,17 +1,24 @@
 public class MaximumProductSubarray {
 	public int maxProduct(int[] nums) {
-		if (nums == null || nums.length == 0) {
-			return 0;
-		}
-		int maxCurr = nums[0];
-		int maxGlobal = maxCurr;
+		if (nums == null || nums.length == 0) return 0;
 
-		for (int i = 1; i < nums.length; i++) {
-			maxCurr = Math.max(nums[i], maxCurr + nums[i]);
-			maxGlobal = Math.max(maxGlobal, maxCurr);
+		int maxVal = nums[0];
+		int maxToCurr = nums[0];
+		int minToCurr = nums[0];
+
+		for (int i = 1; i < nums.length; i ++) {
+
+			int nextMax = maxToCurr * nums[i];
+			int nextMin = minToCurr * nums[i];
+
+			maxToCurr = Math.max(nums[i], Math.max(nextMax, nextMin));
+			minToCurr = Math.min(nums[i], Math.min(nextMax, nextMin));
+
+			maxVal=Math.max(maxVal, maxToCurr);
+
 		}
 
-		return maxGlobal;
+		return maxVal;
 	}
 }
-// https://leetcode.com/problems/maximum-subarray/
+// https://leetcode.com/problems/maximum-product-subarray/
